cbbb4ac8bd4767f9dff2cdc960ffa7e0f22c9862
Maxim Suponya <maxim.suponya@gmail.com>
2016-11-01 13:33:28 +1100

    gitignore & cliean up

--- test.js
+++
@@ -1,7 +0,0 @@
-var auth = require("./AccessControlService");
-
-auth.authenticate("/path/to/no/where", auth.READ, "").then(function(path ){
-    console.log("good "+path);
-}).catch(function () {
-    console.log("not good");
-})
